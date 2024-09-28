54b8e1b2845fc4fde93affaf3f58d90d0d01f5f6
Alma <isakvent@outlook.com>
2024-09-28 12:00:14 +0200

    solve excessive rerendering error

--- src/pages/scrollPilot/ScrollPilot1.tsx
+++ src/pages/scrollPilot/ScrollPilot1.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Styles from "./ScrollPilot.module.css";
 import { ReactComponent as HighLighterAsset } from "../../assets/highlighter.svg";
 import { ReactComponent as ClearIcon} from "../../assets/clear-icon.svg";
@@ -12,90 +12,86 @@ const ScrollPilot1: React.FC<ScrollPilot1Props> = ({ globalValue1, setGlobalValu
     const [hasLoaded, setHasLoaded] = useState(false);
     const [localValue1, setLocalValue1] = useState<number | null>(null);
     const [localLibrary, setLocalLibrary] = useState<(number | null)[]>([]);
-
-    // test
+//    const [scrollLocation, setScrollLocation] = useState(0);
     const scrollContainerRef = useRef<HTMLDivElement | null>(null);
     const localLibraryItemsRef = useRef<(HTMLDivElement | null)[]>([]);
-    const [scrollLocation, setScrollLocation] = useState(0);
 
-    const handleScroll = () => {
-        if (scrollContainerRef.current) {
-            const container = scrollContainerRef.current;
-            const { scrollTop } = container;
-            setScrollLocation(scrollTop); // user's current scroll location
-        
-            localLibrary.forEach((value: number | null, index: number) => {
-                const item = localLibraryItemsRef.current[index]; // Access the specific item ref
-                if (item) {
-                    const { offsetTop } = item; // Retrieve specific information
-                    const closestValue = offsetTop - scrollTop - 100;
-                    if (closestValue < 2 && closestValue > -2) {
-                        console.log(value, "is in focus")
-                    };
-                    if (closestValue < 25 && closestValue > -26) {
-                        console.log("closest item", item.id, "with index", index);
-                        handleSetValue(value, index);
-                    };
-                };
-            });
-        };
-    };
-
-    useEffect(() => {
-        setLocalLibrary([0, 1, -3, 7, 8, -9, 23]);
-        setLocalValue1(globalValue1);
-        setHasLoaded(true);
-
-        const container = scrollContainerRef.current;
-        let timeoutId: ReturnType<typeof setTimeout>;
-
-        const scrollListener = () => {
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-            };
-            timeoutId = setTimeout(() => {
-                handleScroll();
-            }, 250);
-        };
-        if (container) {
-            container.addEventListener("scroll", scrollListener);
-        }
-        return () => {
-            if (container) {
-                container.removeEventListener("scroll", scrollListener);
-            }
-        };
-
-    }, [globalValue1]);
-
-    const handleSetValue = (value: number | null, index: number ) => {
-
+    const handleSetValue = useCallback((value: number | null, index: number ) => {
         const scrollableDiv = document.getElementById((index - 2).toString());
         if (scrollableDiv) {
             scrollableDiv.scrollIntoView({behavior: "smooth"})
         };
         setLocalValue1(value);
         setGlobalValue1(value);
-    };
+    }, [setGlobalValue1]);
+
+    const handleScroll = useCallback(() => {
+        if (scrollContainerRef.current) {
+            const container = scrollContainerRef.current;
+            const { scrollTop } = container;
+//            setScrollLocation(scrollTop);
+        
+            localLibrary.forEach((value: number | null, index: number) => {
+                const item = localLibraryItemsRef.current[index]; // Access the specific item ref
+                if (item) {
+                    const { offsetTop } = item; // Retrieve specific information
+                    const closestValue = offsetTop - scrollTop - 100;
+//                    if (closestValue < 2 && closestValue > -2) {
+//                        console.log(value, "is in focus")
+//                    };
+                    if (closestValue < 25 && closestValue > -26) {
+//                        console.log("closest item", item.id, "with index", index);
+                        handleSetValue(value, index);
+                    };
+                };
+            });
+        };
+    }, [handleSetValue, localLibrary]);
+
+    useEffect(() => {
+        if (hasLoaded) {
+            const container = scrollContainerRef.current;
+            let timeoutId: ReturnType<typeof setTimeout>;
+    
+            const scrollListener = () => {
+                if (timeoutId) {
+                    clearTimeout(timeoutId);
+                };
+                timeoutId = setTimeout(() => {
+                    handleScroll();
+                }, 500);
+            };
+            if (container) {
+                container.addEventListener("scroll", scrollListener);
+            }
+            return () => {
+                if (container) {
+                    container.removeEventListener("scroll", scrollListener);
+                }
+            };
+        };
+
+        setLocalLibrary([null, 0, 1, -3, 7, 8, -9, 23]);
+        setLocalValue1(globalValue1);
+        setHasLoaded(true);
+
+
+    }, [globalValue1, handleScroll, hasLoaded]);
 
     return(hasLoaded ? <>
 
         <p>Local Value: {localValue1}</p>
 
-    <div ref={scrollContainerRef} className={Styles.AlignScrollContainer}>
+        <div ref={scrollContainerRef} className={Styles.AlignScrollContainer}>
 
-        <div className={Styles.HighlighterFrame}>
-             <HighLighterAsset className={Styles.Highlighter}/>
-        </div>
+            <div className={Styles.HighlighterFrame}>
+                <HighLighterAsset className={Styles.Highlighter}/>
+            </div>
 
             <div className={Styles.ScrollContainer}>
 
-                <button id="-3" className={Styles.LibraryItem}/>
                 <button id="-2" className={Styles.LibraryItem}/>
-
-                <button id="-1" className={Styles.LibraryItem} onClick={() => handleSetValue(null, -1)}>
-                    <ClearIcon className={Styles.ClearIcon}/>
-                </button>
+                <button id="-1" className={Styles.LibraryItem}/>
                 {localLibrary.map((id, index) => (
                     <div
                     key={`${id}-${index}`}
@@ -106,17 +102,14 @@ const ScrollPilot1: React.FC<ScrollPilot1Props> = ({ globalValue1, setGlobalValu
                             className={Styles.LibraryItem}
                             id={index.toString()}
                             onClick={() => {handleSetValue(id, index)}}
-                        >{id}</button>
+                        >{id === null ?  <ClearIcon className={Styles.ClearIcon}/> : id}</button>
                     </div>
                 ))}
 
                 <button className={Styles.LibraryItem}/>
                 <button className={Styles.LibraryItem}/>
             </div>
-    </div>
-
-                <p>scrollLocation: {scrollLocation}</p>
-    <button onClick={() => {handleScroll()}}>Check</button>
+        </div>
 
     </> : <p>loading</p>)
 };
